a29b9eb114a035c641e2eaf387b43ad4ce3cf6b4
Joshua Crum <joshua.crum@gmail.com>
2019-05-29 17:34:32 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,12 +3,12 @@ class Backer
   attr_reader :name
   attr_accessor :backed_projects
 
-  def initialize (name)
+  def initialize(name)
     @name = name
     @backed_projects = []
   end
 
-  def backed_projects (project)
+  def back_project(project)
     backed_projects << project
     project.backers << self
   end
